01d5f94c3ed79d7dad73937cc861c454e94e30d1
jantekb <jantekb@gmail.com>
2014-01-04 13:55:34 +0100

    Initial commit

---
+++ src/main/java/hu/javakurzus/bytemap/Bytemap.java
@@ -0,0 +1,5 @@
+package hu.javakurzus.bytemap;
+
+public class Bytemap {
+
+}
---
+++ src/test/java/hu/javakurzus/bytemap/BytemapTest.java
@@ -0,0 +1,12 @@
+package hu.javakurzus.bytemap;
+
+import org.junit.Test;
+
+public class BytemapTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
