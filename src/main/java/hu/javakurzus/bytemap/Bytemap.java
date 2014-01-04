5296df99e0cf8137391672f3bb035f5109214a41
jantekb <jantekb@gmail.com>
2014-01-04 15:27:33 +0100

    Basic homogeneous line representation

--- src/main/java/hu/javakurzus/bytemap/Bytemap.java
+++ src/main/java/hu/javakurzus/bytemap/Bytemap.java
@@ -2,4 +2,56 @@ package hu.javakurzus.bytemap;
 
 public class Bytemap {
 
+	private final int width;
+	
+	private final int height;
+
+	private int[][] rows;
+
+	private static int MAX_HOMO_LENGTH = (int) Math.pow(2, 24) - 1;
+
+	public Bytemap(int width, int height) {
+		this.width = width;
+		this.height = height;
+
+		initialize();
+
+	}
+
+	private void initialize() {
+		this.rows = new int[height][];
+
+		for (int i = 0; i < height; i++) {
+
+			rows[i] = new int[] { 0 };
+		}
+	}
+
+	public int getHeigth() {
+		return height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+
+	public int[] getHomoLine(int length, byte value) {
+		if (length < MAX_HOMO_LENGTH) {
+			return new int[] { (length << 8) + value };
+		} else {
+			int[] remaining = getHomoLine(length - MAX_HOMO_LENGTH, value);
+			int[] result = new int[remaining.length + 1];
+			System.arraycopy(remaining, 0, result, 1, remaining.length);
+			result[0] = (MAX_HOMO_LENGTH << 8) | value;
+			return result;
+		}
+	}
+
+	public int parseLength(int data) {
+		return data >>> 8;
+	}
+
+	public byte parseValue(int data) {
+		return (byte) (data & 0xFF);
+	}
 }
--- src/test/java/hu/javakurzus/bytemap/BytemapTest.java
+++ src/test/java/hu/javakurzus/bytemap/BytemapTest.java
@@ -1,12 +1,66 @@
 package hu.javakurzus.bytemap;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class BytemapTest {
 
+	private Bytemap classUnderTest;
+
+	@Before
+	public void setup() {
+		classUnderTest = new Bytemap(100, 100);
+	}
+
 	@Test
-	public void test() {
-		
+	public void testWidthAndHeightSetting() {
+		Bytemap map = new Bytemap(1, 2);
+
+		assertThat(map.getHeigth(), equalTo(2));
+		assertThat(map.getWidth(), equalTo(1));
+	}
+
+	@Test
+	public void testGetHomoLineForValuesThatFit24bits() {
+		int[] line = classUnderTest.getHomoLine(100, (byte) 1);
+
+		assertThat(line.length, equalTo(1));
+		assertThat(classUnderTest.parseLength(line[0]), equalTo(100));
+		assertThat(classUnderTest.parseValue(line[0]), equalTo((byte) 1));
+
+		line = classUnderTest.getHomoLine(0, (byte) 0);
+		assertThat(line.length, equalTo(1));
+		assertThat(classUnderTest.parseLength(line[0]), equalTo(0));
+		assertThat(classUnderTest.parseValue(line[0]), equalTo((byte) 0));
+
+		line = classUnderTest.getHomoLine(42, (byte) 13);
+		assertThat(line.length, equalTo(1));
+		assertThat(classUnderTest.parseLength(line[0]), equalTo(42));
+		assertThat(classUnderTest.parseValue(line[0]), equalTo((byte) 13));
+	}
+
+	@Test
+	public void testGetHomoLineForValuesThatExceeds24bits() {
+		int biglength = 16777216;
+
+		int[] line = classUnderTest.getHomoLine(biglength, (byte) 1);
+
+		assertThat(line.length, equalTo(2));
+
+		int segment1 = classUnderTest.parseLength(line[0]);
+		int segment2 = classUnderTest.parseLength(line[1]);
+
+		assertThat(
+				segment1
+						+ segment2,
+				equalTo(16777216));
+
+		assertThat(classUnderTest.parseValue(line[0]), equalTo((byte) 1));
+		assertThat(classUnderTest.parseValue(line[1]), equalTo((byte) 1));
+
 	}
 
 }
